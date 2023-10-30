98f8047ea5590aea8b7cc45b08279f53ad01cc3e
Mario <mario.castilloarenas@gmail.com>
2023-10-30 11:02:13 +0100

    created random number on thread

---
+++ src/Hilo.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class Hilo extends Thread {
+    Random random = new Random();
+    private boolean numero = random.nextBoolean();
+    public void run (){
+        Random radnom = new Random();
+        numero = random.nextBoolean();
+
+        if (numero == true){
+            System.out.println(numero + "cara");
+        } else {
+            System.out.println(numero + "cruz");
+        }
+
+    }
+
+
+}
